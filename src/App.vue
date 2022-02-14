ba7d9cb4631093a9ed3e6614bd61bd3625544f08
liubai-a <errgou@gmail.com>
2022-02-14 14:44:03 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>hei</div>
+  <FFF></FFF>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import FFF from './components/FFF.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    FFF
+  }
+}
+</script>
