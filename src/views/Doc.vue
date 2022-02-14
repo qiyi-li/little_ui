18c50f452e85549f9e471190a5a29f199c8beef2
qiyi.li <errgou@gmail.com>
2022-02-14 22:14:45 +0800

    init aside‘s html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,16 +3,30 @@
     <TopNav/>
     <div class="content">
       <aside>
-        边栏
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
       </aside>
       <main>
-        main
+        主内容
       </main>
     </div>
   </div>
 </template>
 <script lang="ts">
-import TopNav from "../components/TopNav.vue";
+import TopNav from '../components/TopNav.vue';
 
 export default {
   components: {TopNav}
