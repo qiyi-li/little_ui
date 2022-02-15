fabd49ee931c08f46e9bbdcb8f97abff5e6c9553
qiyi.li <errgou@gmail.com>
2022-02-15 22:26:42 +0800

    button 基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -10,29 +10,45 @@
 <script lang="ts">
 export default {
   props: {
-    theme: {type: String, default: "button"}
+    theme: {type: String, default: 'button'}
   }
 };
 </script>
 
 <style lang="scss" scoped>
-&::-moz-focus-inner {
-  border: 0;
-}
-&.theme-link{
-  border-color: transparent;
-  box-shadow: none;
-  color: $blue;
-  &:hover,&:focus{
-    color: lighten($blue, 10%);
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.default-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+
+  & + & {
+    margin-left: 8px;
   }
-}
-&.theme-text{
-  border-color: transparent;
-  box-shadow: none;
-  color: inherit;
-  &:hover,&:focus{
-    background: darken(white, 5%);;
+
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+    outline: none;
+  }
+
+  &::-moz-focus-inner {
+    border: 0;
   }
 }
 </style>
