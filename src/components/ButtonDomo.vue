8ae1a463bff847409d7ee09b55e788f4fbadbb71
qiyi <errgou@gmai.com>
2022-02-15 18:12:32 +0800

    支持外部传参数

--- src/components/ButtonDomo.vue
+++ src/components/ButtonDomo.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
     <h3>示例1</h3>
-    <Button>嘿嘿嘿</Button>
+    <Button @click="onClick"
+            @hover="onClick"
+            @focus="onClick"
+            size="small"
+    >嘿嘿嘿
+    </Button>
   </div>
 </template>
 <script lang="ts">
 import Button from "../lib/Button.vue";
 
 export default {
-  components: {Button}
+  components: {Button},
+  setup() {
+    const onClick = () => {console.log("click");};
+    return {onClick};
+  }
 };
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,21 @@
 <template>
-  <button>
+  <button v-bind="rest">
+    <!--    使用v-bind 绑定属性，$attrs表示所有外部传进来的属性-->
     <slot/>
   </button>
 </template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false, //可以取消外部的默认绑定
+  setup(props, context) {  //context.attrs 可以获取到外部传进来的属性
+    const {attrs} = context;
+    const {size, ...rest} = attrs;
+    return {size, rest};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
