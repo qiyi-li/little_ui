79abcf9dfaf14d2d40c357cef266b6abc802fa82
qiyi <errgou@gmai.com>
2022-02-15 17:35:34 +0800

    创建Button

--- src/components/ButtonDomo.vue
+++ src/components/ButtonDomo.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    button
+    <h3>示例1</h3>
+    <Button>嘿嘿嘿</Button>
   </div>
 </template>
+<script lang="ts">
+import Button from "../lib/Button.vue";
+
+export default {
+  components: {Button}
+};
+</script>
