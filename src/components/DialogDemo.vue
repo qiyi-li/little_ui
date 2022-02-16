6b7662482df9182226c602dc5368b34e79657dd2
qiyi <errgou@gmai.com>
2022-02-16 15:35:32 +0800

    create dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    dialog
+    <Dialog/>
   </div>
 </template>
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+
+export default {
+  components: {Dialog}
+
+};
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <header>title</header>
+    <main>
+      <p>one line</p>
+      <p>two line</p>
+    </main>
+    <footer>
+      <Button>OK</Button>
+      <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+
+export default {
+  components: {Button}
+};
+</script>
