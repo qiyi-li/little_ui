08036a5039310b3ad17666f9bdbf4474ff63fc2e
qiyi <errgou@gmai.com>
2022-02-17 15:57:37 +0800

    基本样式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+  <div class="lu-tabs">
+    <div class="lu-tabs-nav">
+      <div class="lu-tabs-nav-item" v-for="(t,index) in titles" :key="index">
+        {{ t }}
+      </div>
+    </div>
+    <div class="lu-tabs-content">
+      <component class="lu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+    </div>
   </div>
 </template>
 
@@ -16,10 +22,37 @@ export default {
         throw new Error("Tabs内子标签必须是Tab");
       }
     });
-    const titles = defaults.map(tab=>{
-      return tab.props.title
-    })
-    return {defaults,titles};
+    const titles = defaults.map(tab => {
+      return tab.props.title;
+    });
+    return {defaults, titles};
   }
 };
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.lu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
