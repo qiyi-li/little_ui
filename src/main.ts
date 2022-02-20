c634ba88c490cbc799a08c6b6b6f5e6feeec32b9
qiyi.li <errgou@gmail.com>
2022-02-20 10:06:43 +0800

    引入样式

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import './lib/lu.scss'
-import './index.scss'
+import './lib/lu.scss';
+import './index.scss';
 import {createApp} from 'vue';
 import App from './App.vue';
 import {router} from './router';
+import 'github-markdown-css';
 
 
 const app = createApp(App);
