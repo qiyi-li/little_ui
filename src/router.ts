69091b62f22eb7726f101aaa79a148428272a5fa
qiyi.li <errgou@gmail.com>
2022-02-20 11:22:11 +0800

    优化markdown的引入代码

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,15 @@
 import {createWebHashHistory, createRouter} from 'vue-router';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
-import Intro from './views/Intro.vue';
-import GetStarted from './views/GetStarted.vue';
-import Install from './views/Install.vue'
 import DocDomo from './components/DocDomo.vue';
 import SwitchDomo from './components/SwitchDomo.vue';
 import ButtonDomo from './components/ButtonDomo.vue';
 import DialogDomo from './components/DialogDemo.vue';
 import TabsDomo from './components/TabsDemo.vue';
+import {h} from 'vue';
+import Markdown from './components/Markdown.vue';
 
+const returnMarkDown = (fileName) => h(Markdown, {path: `../markdown/${fileName}.md`, key: fileName});
 const history = createWebHashHistory();
 export const router = createRouter({
   history,
@@ -18,9 +18,9 @@ export const router = createRouter({
     {
       path: '/doc', component: Doc, children: [
         {path: '', component: DocDomo},
-        {path: 'intro', component: Intro},
-        {path: 'get-started', component: GetStarted},
-        {path: 'install', component: Install},
+        {path: 'intro', component: returnMarkDown('intro')},
+        {path: 'install', component: returnMarkDown('install')},
+        {path: 'get-started', component: returnMarkDown('get-started')},
         {path: 'switch', component: SwitchDomo},
         {path: 'button', component: ButtonDomo},
         {path: 'dialog', component: DialogDomo},
